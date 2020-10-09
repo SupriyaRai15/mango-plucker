c835df769480de5fa5a650382f6b6f8944523375
SupriyaRai15 <69969489+SupriyaRai15@users.noreply.github.com>
2020-10-09 13:16:36 +0530

    Update stone.js

--- stone.js
+++ stone.js
@@ -1,35 +1,34 @@
-class STONE 
+class STONE
 {
-    constructor(x,y,width,height)
-    {
-        var options=
-        {
-           'isStatic' : false,
-           'restitution' : 0,
-           'friction' : 1,
-           'density' : 1.2 
-        }
-           
-     this.body = Bodies.rectangle(x,y,width,height,options);
-     this.width = width;
-     this.height = height;
-     this.x = x;
-     this.y = y;
-     this.image = loadImage("stone.png");
-     World.add(world,this.body);    
+	constructor(x,y,r)
+	{
+		var options={
+			isStatic:false,
+			restitution:0,
+			friction:1,
+			density:1.2
+			}
+		this.x=x;
+		this.y=y;
+		this.r=r
+		this.image=loadImage("stone.png");
+		this.body=Bodies.circle(this.x, this.y, this.r/2, options)
+		World.add(world, this.body);
 
-    }
+	}
+	display()
+	{
+			var stonepos=this.body.position;		
+			push()
+			translate(stonepos.x, stonepos.y);
+			// rectMode(CENTER)
+			// rotate(this.body.angle)
+			fill(255,0,255)
+			imageMode(CENTER);
+			ellipseMode(RADIUS)
+			image(this.image, 0,0,this.r*2, this.r*2)
+			pop()
+			
+	}
 
-
-    display()
-    {
-        push();
-        stroke("white");
-        strokeWeight(4);
-        fill("Red");
-        translate(this.body.position.x, this.body.position.y);
-        imageMode(CENTER);
-        image(this.image,0, 0, this.width, this.height);
-        pop();
-    }
 }
